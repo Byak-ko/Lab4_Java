e85986b265272db49b627fed6dbd45922eb2da63
Byak-ko <95464316+Byak-ko@users.noreply.github.com>
2024-04-24 15:16:25 +0300

    Update Fork.java

--- src/Fork.java
+++ src/Fork.java
@@ -15,16 +15,4 @@ public class Fork {
     public void setName(String name) {
         this.name = name;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Fork fork)) return false;
-        return Objects.equals(getName(), fork.getName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName());
-    }
 }
